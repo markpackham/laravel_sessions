d6699be3a2eea96f01514e3ce543f65f50835162
mark <markpackham1@gmail.com>
2021-12-15 13:29:52 +0000

    add session model
    https://youtu.be/Zap-LNZpvGA?t=1277

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use App\Models\SessionModel;
 
 class PagesController extends Controller
 {
     public function index()
     {
+        $session = SessionModel::all();
 
-        dd(Session::all());
+        // dd(Session::all());
+        dd($session);
 
         return view('index');
     }
