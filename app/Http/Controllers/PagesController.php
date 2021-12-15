656263ec21fcb828ab66d5d7af0ec056576e1c2d
mark <markpackham1@gmail.com>
2021-12-15 12:29:59 +0000

    create new session token
    https://youtu.be/Zap-LNZpvGA?t=848

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,8 +13,19 @@ class PagesController extends Controller
         Session::put('name', ['Bill']);
         Session::put('age', [30, 35, 40]);
 
-        Session::flush();
+        Session::regenerate();
 
+        /*
+  Out put looks like this
+  array:5 [▼
+  "_token" => "UQ5eYxLG3ilZ34jflc51aDtlvrj9HiG2qoIQaQcy"
+  "name" => array:1 [▶]
+  "_previous" => array:1 [▶]
+  "_flash" => array:2 [▶]
+  "age" => array:3 [▶]
+]
+
+        */
         dd(Session::all());
 
         return view('index');
